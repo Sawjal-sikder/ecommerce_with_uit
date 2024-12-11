3975bd9b4f6ee36fc84feefad05f043e277dcf6d
Sawjal_sikder <mdsajalsikder@gmail.com>
2024-12-11 19:05:17 +0600

    product_details to product with id

--- ecommerce/product/views.py
+++ ecommerce/product/views.py
@@ -6,3 +6,8 @@ def product_list(request):
     products = Product.objects.all()
     context = {'products': products}
     return render(request, 'products.html', context)
+
+def product_detail(request, id):
+    product = Product.objects.get(pk=id)
+    context = {'product': product}
+    return render(request, 'product_details.html', context)
