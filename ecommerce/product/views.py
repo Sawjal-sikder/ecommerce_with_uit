ceaa44f0bc318d0bb7448f800a70de63f94924cc
Sawjal_sikder <mdsajalsikder@gmail.com>
2024-12-11 17:55:36 +0600

    Product render template setup

--- ecommerce/product/views.py
+++ ecommerce/product/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Product, Category
 # Create your views here.
+
+def product_list(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'products.html', context)
