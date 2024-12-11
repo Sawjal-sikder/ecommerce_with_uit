497ddb12d59a07199471e37e585a0e02355036db
Sawjal_sikder <mdsajalsikder@gmail.com>
2024-12-11 17:22:10 +0600

    Create Model and Admin in Category and Product

--- ecommerce/product/models.py
+++ ecommerce/product/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    slug = models.SlugField(max_length=100)
+
+
+
+    def save(self, *args, **kwargs):
+        self.slug = self.name.replace(" ", "-").lower()
+        super(Product, self).save(*args, **kwargs)
+        
+    def __str__(self):
+        return self.name
